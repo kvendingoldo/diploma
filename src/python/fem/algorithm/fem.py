25e61b25432398986d03cf5a81df4e41618367d3
Alexander Sharov <kvendingoldo@yandex.ru>
2018-04-18 01:13:58 +0400

    Add H0

--- src/python/fem/algorithm/fem.py
+++ src/python/fem/algorithm/fem.py
@@ -1,44 +1,39 @@
 # -*- coding: utf-8 -*-
 # @Author: Alexander Sharov
 
-import matplotlib.pyplot as plt
 import numpy as np
-# from mpmath import *
 
 from sympy import *
 from sympy import sqrt
-from sympy.plotting import plot3d
 
 from scipy.integrate import odeint
 
 from geometry.point import Point
 from fe.triangle import Triangle
 
-# constants
+# CONSTANTS
 
 # плотность воды [кг / м^3]
 rho = 1000
-
 # плотность воздуха [кг / м^3]
 rho_a = 1.2754
-
 # давление на поверхности воды [Па]
 P_a = 10 ** 5
-
 # скорость ветра [м / c]
 W = 1
-
 # gamma ^ 2
 gamma = 0.002
-
 # ускорение свободного падения [м * c^2]
 g = 9.832
+# g / c^2
+gc2 = 0.002
+# коэффициент трения Шэззи [м^{1/2} * c^{-1}]
+C = 40
 
-# коэффициент трения Шэззи
-C = 1
+H0=0.1
 
 
-def solve(mesh):
+def solve(time, mesh):
     M = mesh.quantity
     elements = mesh.splitting
     x1, x2 = symbols('x_1 x_2')
@@ -54,20 +49,23 @@ def solve(mesh):
             for k in range(0, M):
                 N_k = element.get_basic_function_by_number(k)
                 weight_functions = 0
+
                 for l in range(0, M):
+                    #print('test k=%d, l=%d\n' % (k, l))
                     W_l = element.get_basic_function_by_number(l)
                     weight_functions += W_l
 
                     f_eq1 += \
-                        + element.integrate(W_l * diff(-P_a * variables[2 * M + k] * N_k - g * rho / 2 * variables[2 * M + k] ** 2 * N_k ** 2, x1)) \
-                        + element.integrate(P_a * W_l * diff(variables[2 * M + k] * N_k, x1)) \
-                        + element.integrate(W ** 2 * gamma * rho_a * W_l) \
-                        - element.integrate((g * W_l * variables[k] / C ** 2 * rho * variables[2 * M + k] ** 2 * N_k) * sqrt(variables[k] ** 2 * N_k ** 2 + variables[M + k] ** 2 * N_k ** 2))
+                        + element.integrate(W_l * diff(-P_a * H0 - P_a * variables[2 * M + k] * N_k - ((g * rho / 2) * H0 ** 2) - g * rho * H0 * variables[2 * M + k] * N_k - g * rho / 2 * variables[2 * M + k] ** 2 * N_k ** 2, x1)) \
+                        + element.integrate(P_a * W_l * diff(H0 + variables[2 * M + k] * N_k, x1)) \
+                        + element.integrate(sqrt(2)/2 * W ** 2 * gamma * rho_a * W_l) \
+                        - element.integrate((gc2 * W_l * variables[k] / (rho * H0 ** 2) * variables[2 * M + k] ** 2 * N_k) * sqrt(variables[k] ** 2 * N_k ** 2 + variables[M + k] ** 2 * N_k ** 2))
 
                     f_eq2 += \
-                        + element.integrate(W_l * diff(-P_a * variables[2 * M + k] * N_k - g * rho / 2 * variables[2 * M + k] ** 2 * N_k ** 2, x2)) \
+                        + element.integrate(W_l * diff(-P_a * H0 - P_a * variables[2 * M + k] * N_k - ((g * rho / 2) * H0 ** 2) - g * rho * H0 * variables[2 * M + k] * N_k - g * rho / 2 * variables[2 * M + k] ** 2 * N_k ** 2, x2)) \
                         + element.integrate(P_a * W_l * diff(variables[2 * M + k] * N_k, x2)) \
-                        - element.integrate((g * W_l * variables[M + k] / C ** 2 * rho * variables[2 * M + k] ** 2 * N_k) * sqrt(variables[k] ** 2 * N_k ** 2 + variables[M + k] ** 2 * N_k ** 2))
+                        + element.integrate(sqrt(2)/2 * W ** 2 * gamma * rho_a * W_l) \
+                        - element.integrate((gc2 * W_l * variables[M + k] / (rho * H0 ** 2) * variables[2 * M + k] ** 2 * N_k) * sqrt(variables[k] ** 2 * N_k ** 2 + variables[M + k] ** 2 * N_k ** 2))
 
                     f_eq3 += \
                         - element.integrate(W_l * diff(N_k, x1)) * variables[k] \
@@ -78,22 +76,21 @@ def solve(mesh):
                 coefficient_of_d_eq3 = element.integrate(rho * weight_functions * N_k)
 
                 if coefficient_of_d_eq1 != 0:
+                    #print(coefficient_of_d_eq1)
                     sysfun[k] += (f_eq1 / coefficient_of_d_eq1)
-                # [x / coefficient_of_d_eq1 for x in f_eq2]
 
                 if coefficient_of_d_eq2 != 0:
+                    #print(coefficient_of_d_eq2)
                     sysfun[M + k] += (f_eq2 / coefficient_of_d_eq2)
-                # [x / coefficient_of_d_eq2 for x in f_eq3]
 
                 if coefficient_of_d_eq3 != 0:
+                    #print(coefficient_of_d_eq3)
                     sysfun[2 * M + k] += (f_eq3 / coefficient_of_d_eq3)
-                # [x / coefficient_of_d_eq3 for x in f_eq1]
 
         # print('sysfun=%s\n' % sysfun)
 
         return sysfun
 
-    time = np.linspace(3, 4, 10)
     y0 = np.zeros(3 * M)
     sol = odeint(system, y0, time)
 
@@ -101,24 +98,20 @@ def solve(mesh):
     q2_list = list()
     H_list = list()
 
-    for element in mesh.splitting:
+    for element in elements:
         for ind in range(0, M):
             q1 = 0
             q2 = 0
             H = 0
             for k in range(0, M):
                 N_k = element.get_basic_function_by_number(k)
-                q1 += N_k * sol[ind][M + k]
-                q2 += N_k * sol[ind][k]
+                q1 += N_k * sol[ind][k]
+                q2 += N_k * sol[ind][M + k]
                 H += N_k * sol[ind][2 * M + k]
 
             q1_list.append(q1)
             q2_list.append(q2)
             H_list.append(H)
 
-    print(sol)
-    print(time)
-    # print(q1_list)
-    # print(q2_list)
-    print(H_list)
-    plot3d(H_list[1], (x1, -1, 1), (x2, -1, 1))
+    #print(sol)
+    return q1_list, q2_list, H_list
--- src/python/fem/main.py
+++ src/python/fem/main.py
@@ -1,13 +1,59 @@
 # -*- coding: utf-8 -*-
 # @Author: Alexander Sharov
 
+import numpy as np
+import matplotlib
+
+from sympy import symbols, lambdify
+from sympy.plotting import plot3d
+
+import matplotlib.pyplot as plt
+
 from data import mesh as m
 from algorithm import fem
 
-mesh = m.Mesh('/Users/ashraov/projects/study/diploma/resources/poly/pond_without_islands.poly')
-mesh.generate()
-# print(mesh.splitting)
-#mesh.show()
-fem.solve(mesh)
+
+def main():
+    mesh = m.Mesh('/Users/ashraov/projects/study/diploma/resources/poly/pond_without_islands.poly')
+    #mesh = m.Mesh('/Users/ashraov/projects/study/diploma/resources/poly/lake_svetloyar.poly')
+    mesh.generate()
+    #mesh.show()
+
+    x1, x2 = symbols('x_1 x_2')
+    time = np.linspace(3, 4, 2)
+    q1, q2, H = fem.solve(time, mesh)
 
 
+    print(time)
+    print(q1)
+    print(q2)
+    print(H)
+
+    plot3d(q1[1], (x1, -1, 1), (x2, -1, 1))
+    plot3d(q2[1], (x1, -1, 1), (x2, -1, 1))
+    plot3d(H[1], (x1, -1, 1), (x2, -1, 1))
+
+    f1 = lambdify((x1, x2), q1[1], 'numpy')
+    f2 = lambdify((x1, x2), q2[1], 'numpy')
+
+    a = np.linspace(-7, 7, 1000)
+    b = np.linspace(-5, 5, 1000)
+    x, y = np.meshgrid(a, b)
+
+
+
+
+    fig = plt.figure()
+    ax = fig.gca()
+    plt.grid()
+
+    CS = matplotlib.pyplot.contour(x, y, f1(x, y))
+    #CS = matplotlib.pyplot.contour(x, y, f2(x, y))
+    plt.clabel(CS, fontsize=9, inline=10)
+    plt.show()
+
+
+
+
+if __name__ == "__main__":
+    main()
